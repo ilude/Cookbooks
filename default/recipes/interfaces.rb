579c112529edb27a72cce4593fb1b694b6b54ec9
Mike Glenn <mike.glenn@rammount.com>
2012-06-15 13:27:29 -0400

    remove networking service reference

--- default/recipes/interfaces.rb
+++ default/recipes/interfaces.rb
@@ -1,14 +1,9 @@
-service "networking " do
-  supports :restart => true, :start => true, :stop => true
-end
-
 if node[:network] && node[:network][:static]
   template "/etc/network/interfaces.new" do
     source "interfaces.static.erb"
     owner "root"
     group "root"
     mode "0600"
-    notifies :restart, resources(:service => "networking")
   end
 else
   template "/etc/network/interfaces.new" do
@@ -22,5 +17,4 @@ end
 execute "replace_interfaces_file" do
   command "mv /etc/network/interfaces.new /etc/network/interfaces"
   action :run
-  notifies :restart, resources(:service => "networking")
 end
