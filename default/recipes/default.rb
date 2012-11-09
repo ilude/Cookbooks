a6c832df1fc78bea1735695181fce73dac3d86a7
Mike Glenn <mglenn@ilude.com>
2012-11-08 22:25:04 -0500

    update aliase

--- default/recipes/default.rb
+++ default/recipes/default.rb
@@ -3,6 +3,6 @@ include_recipe "ssmtp"
 package "tmux" 
 
 execute "add_aliases" do
-  command "echo alias l='ls -la' >> /etc/bash.bashrc"
-  not_if { File.read("/etc/bash.bashrc").include?("alias l='ls -la'") }
+  command "echo alias l=\'ls -la\' >> /etc/bash.bashrc"
+  not_if { File.read("/etc/bash.bashrc").include?("alias l=\'ls -la\'") }
 end
