1ad44c24b6f302e7301727cef992786c7192b073
Mike Glenn <mike.glenn@rammount.com>
2013-03-06 12:37:28 -0500

    add sudo setup to default recipe

--- default/recipes/default.rb
+++ default/recipes/default.rb
@@ -22,6 +22,11 @@ file "/root/.ssh/id_rsa" do
   action :create_if_missing
 end 
 
+execute "sudo setup" do
+  command "/bin/sed -i -e 's/%admin ALL=NOPASSWD:ALL/%adm ALL=NOPASSWD:ALL/g' /etc/sudoers"
+  only_if { File.read("/etc/sudoers").include?("%admin ALL=NOPASSWD:ALL")}
+end
+
 #execute "add_aliases" do
 # command "echo alias l=\'ls -la\' >> /etc/bash.bashrc"
 # not_if { File.read("/etc/bash.bashrc").include?("alias l=\'ls -la\'") }
