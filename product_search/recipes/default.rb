76e685e1ec24d0237939d1c515ecb4446fa9953f
Mike Glenn <mglenn@ilude.com>
2013-07-22 23:11:50 -0400

    update product search recipes

--- product_search/recipes/default.rb
+++ product_search/recipes/default.rb
@@ -16,10 +16,14 @@ host = "bitbucket.org"
 repo = "git@#{host}:rammounts/product_search.git"
 known_hosts = "/home/#{node[:unicorn][:user]}/.ssh/known_hosts"
 
-execute "add_known_host" do
-  command "ssh-keyscan -t rsa #{host} >> #{known_hosts}"
-  user node[:unicorn][:user]
-  group node[:unicorn][:group]
+bash "add_known_host" do
+  user  node[:unicorn][:user]
+  cwd   "/home/#{node[:unicorn][:user]}"
+  code  <<-EOH
+  ssh-keyscan -t rsa #{host} >> #{known_hosts}
+  chown #{node[:unicorn][:user]}:#{node[:unicorn][:group]} #{known_hosts}
+  chmod 600 #{known_hosts}
+  EOH
   not_if { File.exists?(known_hosts) && File.read(known_hosts).include?(host) }
 end
 
@@ -31,14 +35,14 @@ git "#{node[:unicorn][:apps_dir]}/#{app_name}" do
   group node[:unicorn][:group]
 end
 
-execute "bundler" do
-  command "bundle install --no-deployment"
-  cwd File.join(node[:unicorn][:apps_dir], app_name)
-  action :run
-end
+# execute "bundler" do
+#   command "bundle install --no-deployment"
+#   cwd File.join(node[:unicorn][:apps_dir], app_name)
+#   action :run
+# end
 
 execute "deployment bundler" do
-  command "bundle install --deployment"
+  command "bundle install --deployment --without development test"
   user node[:unicorn][:user]
   group node[:unicorn][:group]
   cwd File.join(node[:unicorn][:apps_dir], app_name)
@@ -52,7 +56,9 @@ end
 # end
 
 execute "assets precompile" do
-  command "sudo -u #{node[:unicorn][:user]} bundle exec rake assets:precompile"
+  command "bundle exec rake assets:precompile"
+  user node[:unicorn][:user]
+  group node[:unicorn][:group]
   cwd File.join(node[:unicorn][:apps_dir], app_name)
   action :run
 end
