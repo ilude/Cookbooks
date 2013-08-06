6bcf4b605453bc96aa156f56b5580b5fdd19b5d6
Mike Glenn <mike.glenn@rammount.com>
2013-08-06 14:55:28 -0400

    add logrotate configuration to unicorn install

--- unicorn/recipes/default.rb
+++ unicorn/recipes/default.rb
@@ -2,4 +2,13 @@ include_recipe "nginx"
 
 gem_package "unicorn"
 
+# create logrotate configuration for all rails apps
+template "rails" do
+  path "/etc/logrotate.d/rails"
+  source "rails.logrotate.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
 include_recipe "unicorn::user"
