c981e230979ab67f3159d0d2c2f5c49c259b3392
Mike Glenn <mike.glenn@rammount.com>
2014-04-09 15:05:27 -0400

    renamed profit report to shipped report for the shipped report cronjob

--- reports/recipes/shipped_profit_report.rb
+++ reports/recipes/shipped_profit_report.rb
@@ -34,7 +34,7 @@ execute "deployment bundler" do
   action :run
 end
 
-cron "profit report" do
+cron "shipped report" do
   hour "6"
   minute "06"
   command "cd #{node[:unicorn][:apps_dir]}/#{app_name}; /usr/local/bin/bundle exec /usr/local/bin/ruby profit_report.rb -o /apps/reports/#{app_name} -s open"
